babf3a628f9300b58ece359db07cba1e4316e30c
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2025-11-19 13:22:06 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 
 import React, { useEffect } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { CartProvider } from './context/CartContext';
 import { ProductProvider } from './context/ProductContext';
 import { AppLayout } from './components/layout/AppLayout';
@@ -16,13 +17,15 @@ function App() {
   }, []);
 
   return (
-    <CartProvider>
-      <ProductProvider>
-        <HashRouter>
-          <AppLayout />
-        </HashRouter>
-      </ProductProvider>
-    </CartProvider>
+    <HelmetProvider>
+      <CartProvider>
+        <ProductProvider>
+          <BrowserRouter>
+            <AppLayout />
+          </BrowserRouter>
+        </ProductProvider>
+      </CartProvider>
+    </HelmetProvider>
   );
 }
 
