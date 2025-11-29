684b7c4b3cf5136e96cc37b502b8d4ebb9cad608
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2025-11-29 18:58:27 +0100

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -69,8 +69,11 @@ export const PRODUCTS: Product[] = [
     requiredPhotos: 6, // Default, will be updated by variant
     variants: [
       { id: '5x5', name: '5x5 cm', photoCount: 1, price: 15, imageUrl: 'https://i.imgur.com/1oIzU4r.jpeg' },
-      { id: '10x10', name: '10x10 cm', photoCount: 1, price: 25, imageUrl: 'https://i.imgur.com/7U3iv2e.jpeg' },
+      { id: '7x7', name: '7x7 cm', photoCount: 1, price: 20, imageUrl: 'https://i.imgur.com/1oIzU4r.jpeg' },
       { id: '5x10', name: '5x10 cm', photoCount: 1, price: 20, imageUrl: 'https://i.imgur.com/miugWFP.jpg' },
+      { id: '10x10', name: '10x10 cm', photoCount: 1, price: 25, imageUrl: 'https://i.imgur.com/7U3iv2e.jpeg' },
+      { id: '9x13', name: '9x13 cm', photoCount: 1, price: 30, imageUrl: 'https://i.imgur.com/miugWFP.jpg' },
+      { id: 'custom-a5', name: 'Libovolný rozměr < A5', photoCount: 1, price: 50, imageUrl: 'https://i.imgur.com/miugWFP.jpg' },
       { id: 'a5-sheet', name: 'A5', photoCount: 1, price: 70, imageUrl: 'https://i.imgur.com/miugWFP.jpg' },
       { id: 'a4-sheet', name: 'A4', photoCount: 1, price: 95, imageUrl: 'https://i.imgur.com/miugWFP.jpg' },
     ]
