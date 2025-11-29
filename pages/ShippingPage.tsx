3b3889f85d5d276539b732262156aa3161a045ea
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2025-11-29 23:46:04 +0100

    Update ShippingPage.tsx

--- pages/ShippingPage.tsx
+++ pages/ShippingPage.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { PageWrapper, SectionTitle } from '../components/layout/PageWrapper';
 
@@ -7,7 +8,7 @@ const ShippingPage: React.FC = () => (
         <p>Každý produkt je vyráběn na zakázku s maximální péčí. Doba výroby je obvykle 3-5 pracovních dnů od přijetí platby. Po dokončení výroby je zásilka předána dopravci.</p>
         <SectionTitle>Možnosti dopravy</SectionTitle>
         <ul className="list-disc pl-6 space-y-2">
-            <li><strong>Zásilkovna - Výdejní místo:</strong> 72 Kč (Doručení obvykle do 2 pracovních dnů od expedice)</li>
+            <li><strong>Zásilkovna - Výdejní místo:</strong> 89 Kč (Doručení obvykle do 2 pracovních dnů od expedice)</li>
             <li><strong>Česká pošta - Balík Do ruky:</strong> 119 Kč (Doručení na Vaši adresu, obvykle do 2 pracovních dnů od expedice)</li>
             <li><strong>Osobní odběr - Turnov:</strong> Zdarma (Po předchozí domluvě)</li>
         </ul>
