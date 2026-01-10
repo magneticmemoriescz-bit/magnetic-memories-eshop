adffea1a3faa0b7276234187c62b1f40831c61e7
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2026-01-10 20:35:11 +0100

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,8 @@ export {};
 declare global {
     interface Window {
         Packeta: any;
+        BalikovnaWidget: any;
+        pplParcelShopWidget: any;
         uploadcare: any;
         emailjs: any;
         gtag: (...args: any[]) => void;
