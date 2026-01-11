91947318ba12eb978e32e02b38f0b01bcdb0922b
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2026-01-11 16:41:14 +0100

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -3,9 +3,24 @@ export {};
 
 declare global {
     interface Window {
-        Packeta: any;
-        BalikovnaWidget: any;
-        pplParcelShopWidget: any;
+        Packeta: {
+            Widget: {
+                pick: (apiKey: string, callback: (point: any) => void, options?: any) => void;
+            };
+        };
+        BalikovnaWidget: {
+            open: (callback: (point: any) => void) => void;
+        };
+        pplParcelShopWidget: {
+            open: (callbackOrConfig: any) => void;
+        };
+        pplWidget: {
+            open: (callbackOrConfig: any) => void;
+        };
+        PPLWidget: {
+            open: (callbackOrConfig: any) => void;
+        };
+        balikovnaWidget: any;
         uploadcare: any;
         emailjs: any;
         gtag: (...args: any[]) => void;
