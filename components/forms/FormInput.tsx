77f0b41f4eec3b6974253976e98d5ed1c712b7d3
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2026-01-17 22:34:47 +0100

    Update FormInput.tsx

--- components/forms/FormInput.tsx
+++ components/forms/FormInput.tsx
@@ -1,14 +1,18 @@
+
 import React from 'react';
 
-export const FormInput = ({ name, label, error, value, onChange, ...props }: any) => (
+export const FormInput = ({ name, label, error, value, onChange, required, ...props }: any) => (
     <div>
-        <label htmlFor={name} className="block text-sm font-medium text-gray-700">{label}</label>
+        <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+            {label} {required && <span className="text-red-500">*</span>}
+        </label>
         <input 
             id={name} 
             name={name} 
             {...props} 
             value={value}
             onChange={onChange}
+            required={required}
             className={`mt-1 block w-full border rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-brand-purple focus:border-brand-purple sm:text-sm placeholder-gray-500 ${error ? 'border-red-500 bg-red-50' : 'border-brand-purple/20 bg-brand-purple/10'}`}
         />
         {error && <p className="mt-1 text-sm text-red-500">{error}</p>}
